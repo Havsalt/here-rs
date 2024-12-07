5e2d70ec650be3fc0f37124385de5aa79ab0f5e3
Havsalt <knutolaihelgesen@gmail.com>
2024-12-07 20:09:37 +0100

    Added --completions and --markdown

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::io;
 use std::path::PathBuf;
 use std::process::ExitCode;
 use std::{env::current_dir, fs};
 
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+use clap_complete::{generate, Shell};
+use clap_markdown::print_help_markdown;
 use cli_clipboard;
 use colored::Colorize;
 use enigo::{Enigo, Key, Keyboard, Settings};
@@ -63,10 +66,67 @@ struct Args {
         help = "Select first option if multiresult (when: -w/--from-where)"
     )]
     select_first_option: bool,
+    #[arg(long = "completions",
+        value_name = "SHELL",
+        help = "Generate completions for given shell",
+        conflicts_with_all = [
+            "folder_component",
+            "where_search",
+            "change_directory",
+            "escape_backslash",
+            "wrap_quote",
+            "resolve_symlink",
+            "no_copy",
+            "no_color",
+            "posix_style",
+            "no_posix_style",
+            "select_first_option",
+            "generate_markdown",
+            "segment_or_name"
+        ]
+    )]
+    pub generate_completions: Option<Shell>,
+    #[arg(
+        long = "markdown",
+        help = "Generate markdown help page",
+        conflicts_with_all = [
+            "folder_component",
+            "where_search",
+            "change_directory",
+            "escape_backslash",
+            "wrap_quote",
+            "resolve_symlink",
+            "no_copy",
+            "no_color",
+            "posix_style",
+            "no_posix_style",
+            "select_first_option",
+            "generate_completions",
+            "segment_or_name"
+        ]
+    )]
+    pub generate_markdown: bool,
 }
 
 fn main() -> ExitCode {
     let args = Args::parse();
+
+    // Generate completions and markdown help page
+    if let Some(shell) = args.generate_completions {
+        generate(
+            shell,
+            &mut Args::command(),
+            Args::command().get_name().to_string(),
+            &mut io::stdout(),
+        );
+    }
+    if args.generate_markdown {
+        print_help_markdown::<Args>();
+    }
+    // Abort after printing completions or markdown help page
+    if args.generate_completions.is_some() || args.generate_markdown {
+        return ExitCode::SUCCESS;
+    }
 
     // Select where to extract the path from
     let mut path = if args.where_search {
@@ -147,7 +207,7 @@ fn main() -> ExitCode {
     if args.wrap_quote {
         visual = format!("\"{}\"", visual)
     }
-    
+
     if args.escape_backslash {
         visual = visual.replace("\\", "\\\\")
     }
